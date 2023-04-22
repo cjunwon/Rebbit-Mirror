733ff1778250702ad628f4d9924c7e3d2a4a1571
Jason Lim <jmlim@pm.me>
2023-04-22 13:30:54 -0700

    Integration test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
-from praw import Reddit
+import reddit_data
+import generate_prompt
+import image_generation
 
 
 def main():
     print("cock n balls")
+    username = "brokenjago"
+    print("query reddit")
+    comments = reddit_data.getAllUserComments(username=username, count=5)
+    posts = reddit_data.getAllUserPosts(username=username, count=5)
+    print(comments)
+    print(username)
+    cleaned_comments = [(comment["submission_or_parent_content"],
+                         comment["comment_content"]) for comment in comments]
+    cleaned_posts = [
+        (post["submission_title"], post["submission_content"]) for post in posts]
+    response_beginning = "This person is"
+    print("create cohere prompt")
+    prompt = generate_prompt.format_prompt(
+        cleaned_comments, cleaned_posts, response_beginning)
+    print(prompt)
+    print("query cohere")
+    image_gen_prompt = generate_prompt.generate_prompt(prompt)
+    print(image_gen_prompt)
+    url = image_generation.generate_image(image_gen_prompt)
+    print(url)
+
 
 if __name__ == '__main__':
     main()
